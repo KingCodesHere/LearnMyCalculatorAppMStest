e7d454ae21aa9455ba68988bcd685ccdf6f98f57
Nicholas <Nicholas@SIL3-08>
2023-12-20 15:06:09 +1100

    Add project files.

---
+++ LearnMyCalculatorApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace LearnMyCalculatorApp
+{
+	public class Calculator
+	{
+		public int Add(int x, int y)
+		{
+			return x + y;
+		}
+
+		public int Subtract(int x, int y)
+		{
+			return x - y;
+		}
+
+		public int Multiply(int x, int y)
+		{
+			return x * y;
+		}
+
+		public int Divide(int x, int y)
+		{
+			return x / y;
+		}
+	}
+}
---
+++ LearnMyCalculatorAppTests/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnMyCalculatorApp;
+
+namespace LearnMyCalculatorApp.Tests
+{
+	[TestClass()]
+	public class CalculatorTests
+	{
+		[TestMethod()]
+		public void AddTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void SubtractTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void MultiplyTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void DivideTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
